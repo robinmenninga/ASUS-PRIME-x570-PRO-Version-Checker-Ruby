d615bd24c6b7e2b98f07c2f347b974595435aae8
Robin Menninga <r.menninga@live.nl>
2021-03-19 20:09:06 +0100

    Changed text files to single json file

--- check_bios.rb
+++ check_bios.rb
@@ -3,32 +3,36 @@ require 'json'
 
 
 def checkForUpdates(toCheck)
-    if toCheck == 'chipset' then
-        textfile = 'chipsetversion.txt'
+    case toCheck
+    when 'chipset'
         option = 2
-    elsif toCheck == 'bios' then
-        textfile = 'biosversion.txt'
+    when 'bios'
         option = 0
     else
         puts 'Wrong parameter, choose between \'bios\' and \'chipset\''
     end
-    
-    if File.exist?(textfile) then
-        current = File.open(textfile).read
-        link = 'https://www.asus.com/support/api/product.asmx/GetPDDrivers?website=us&model=PRIME-X570-PRO&pdhashedid=aDvY2vRFhs99nFdl&cpu=&osid=45'
-        newest = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][option]['Files'][0]['Version']
-        if current.tr('.', '') < newest.tr('.', '') then
-            puts "There is a newer #{toCheck} available!"
-            puts "Current #{toCheck} version: #{current}"
-            puts "Newest #{toCheck} version: #{newest}"
-            puts "Would you like to update the version file to the newest #{toCheck}? You can always do this manually later. (y/n)"
-            if gets.chomp == 'y' then File.open(textfile, 'w') { |file| file.write(newest) } end
-        else puts "You have the latest #{toCheck}." end
-    else 
-        File.open(textfile, 'w') { |file| file.write('0') }
-        puts "Please enter your current #{toCheck} version in the newly made file \'#{textfile}\'"
-    end
+
+    json = JSON.parse(File.read('version.json'))
+    current = json[toCheck]
+    link = 'https://www.asus.com/support/api/product.asmx/GetPDDrivers?website=us&model=PRIME-X570-PRO&pdhashedid=aDvY2vRFhs99nFdl&cpu=&osid=45'
+    newest = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][option]['Files'][0]['Version']
+    if current.tr('.', '') < newest.tr('.', '') then
+        puts "There is a newer #{toCheck} available!"
+        puts "Current #{toCheck} version: #{current}"
+        puts "Newest #{toCheck} version: #{newest}"
+        puts "Would you like to update the version file to the newest #{toCheck}? You can always do this manually later. (y/n)"
+        if gets.chomp == 'y' then
+            json[toCheck] = newest
+            File.open('version.json', 'w') { |file| file.write(JSON.dump(json)) }
+        end
+    else puts "You have the latest #{toCheck}." end
+
 end
 
-checkForUpdates('bios')
-checkForUpdates('chipset')
+if File.exist?('version.json') then
+    checkForUpdates('bios')
+    checkForUpdates('chipset')
+else
+    File.open('version.json', 'w') { |file| file.write(JSON.generate({:bios => "0", :chipset => "0"})) }
+    puts "Please enter your current versions in the newly made file \'version.json\'"
+end
