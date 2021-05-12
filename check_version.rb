107fda1640ea21dc8441c7da5b1733e1a4dc61d0
Robin Menninga <r.menninga@live.nl>
2021-05-12 16:49:44 +0200

    Added audio driver check

--- check_version.rb
+++ check_version.rb
@@ -1,60 +1,96 @@
 require 'httparty'
 require 'launchy'
 
-
-def checkForUpdates(toCheck)
+def getCurrentVersion(toCheck)
     case toCheck
-    when 'chipset'
-        option = 1
-        link = 'https://www.asus.com/support/api/product.asmx/GetPDDrivers?website=us&model=PRIME-X570-PRO&pdhashedid=aDvY2vRFhs99nFdl&osid=45'
-        if File.exist?('C:/AMD/Chipset_Driver_Installer/AMD_Chipset_Software.exe') then
-            current = %x(wmic datafile where 'name="C:\\\\AMD\\\\Chipset_Driver_Installer\\\\AMD_Chipset_Software.exe"' get version).tr("Version \n", '')
-        else
-            puts 'Chipset software exe file not found, skipping chipset version check.'
-            return
-        end
     when 'bios'
-        option = 0
-        link = 'https://www.asus.com/support/api/product.asmx/GetPDBIOS?website=us&model=PRIME-X570-PRO&pdhashedid=aDvY2vRFhs99nFdl'
-        current = %x(wmic bios get name).tr("Name \n", '')
-		unless current =~ /\d/ 
-			puts 'Cannot find current version.'
-			return
-		end
-    else
-        puts 'Wrong parameter, choose between \'bios\' and \'chipset\'.'
+        current = %x(wmic bios get name 2>&1).tr("Name \n", '')
+    when 'chipset'
+        current = %x(wmic datafile where 'name="C:\\\\AMD\\\\Chipset_Driver_Installer\\\\AMD_Chipset_Software.exe"' get version 2>&1).tr("Version \n", '')
+    when 'audiodriver'
+        current = %x(powershell.exe -EncodedCommand "RwBlAHQALQBXAG0AaQBPAGIAagBlAGMAdAAgAFcAaQBuADMAMgBfAFAAbgBQAFMAaQBnAG4AZQBkAEQAcgBpAHYAZQByACAALQBGAGkAbAB0AGUAcgAgACIARABlAHYAaQBjAGUATgBhAG0AZQAgAD0AIAAnAFIAZQBhAGwAdABlAGsAIABIAGkAZwBoACAARABlAGYAaQBuAGkAdABpAG8AbgAgAEEAdQBkAGkAbwAnACIAIAB8ACAAcwBlAGwAZQBjAHQAIABkAHIAaQB2AGUAcgB2AGUAcgBzAGkAbwBuACAAfAAgAEYAbwByAG0AYQB0AC0AVABhAGIAbABlACAALQBIAGkAZABlAFQAYQBiAGwAZQBIAGUAYQBkAGUAcgBzAA==").tr("\n", '')
     end
-    
-    newest = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][option]['Files'][0]['Version']
-    unless newest =~ /\d/
-        puts "Cannot find newest #{toCheck} version."
-        return
+    if current =~ /\d/
+        return current
     end
-    is_release = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][option]['Files'][0]['IsRelease']
-    puts "\t- #{toCheck.upcase} -"
-    if current.tr('.', '') < newest.tr('.', '') 
-        puts "There is a newer #{toCheck} available!"
-        puts "Current #{toCheck} version: #{current}."
-        puts "Newest #{toCheck} version: #{newest}."
-        puts "\n"
-        if (is_release == '0')
-            puts "Warning! This is a beta version."
-        end
-		puts "\n"
-        true
-    else
-        puts "You have the latest #{toCheck}."
-		puts "\n\n"
-    end
+    puts "Current #{toCheck} version not found, skipping...\n\n"
+    return -1
 end
 
-bios = checkForUpdates('bios')
-chipset = checkForUpdates('chipset')
+def getNewestVersion(toCheck)
+    link = 'https://www.asus.com/support/api/product.asmx/GetPDDrivers?website=us&model=PRIME-X570-PRO&pdhashedid=aDvY2vRFhs99nFdl&osid=45'
+    case toCheck
+    when 'bios'
+        link = 'https://www.asus.com/support/api/product.asmx/GetPDBIOS?website=us&model=PRIME-X570-PRO&pdhashedid=aDvY2vRFhs99nFdl'
+        newest = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][0]['Files'][0]['Version']
+    when 'chipset'
+        newest = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][1]['Files'][0]['Version']
+    when 'audiodriver'
+        newest = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][2]['Files'][0]['Version']
+    end
 
-if bios or chipset
+    return newest if newest =~ /\d/
+
+    puts "Newest #{toCheck} version not found, skipping...\n\n"
+    return -1
+end
+
+def isRelease(toCheck)
+    case toCheck
+    when 'bios'
+        is_release = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][0]['Files'][0]['IsRelease']
+    when 'chipset'
+        is_release = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][1]['Files'][0]['IsRelease']
+    when 'audiodriver'
+        is_release = JSON.parse(HTTParty.get(link).body)['Result']['Obj'][2]['Files'][0]['IsRelease']
+    end
+
+    return true if is_release == '1'
+end
+
+def isCheckable(toCheck)
+    checks = ['bios', 'chipset', 'audiodriver']
+    checkable = checks.include? toCheck
+    puts 'Wrong parameter' unless checkable
+    return checkable
+end
+
+def openBrowser
     puts 'Would you like to open your webbrowser to the update page? (Y/n)'
 	answer = gets.chomp
     if answer == 'y' or answer == ''
         Launchy.open("https://www.asus.com/us/Motherboards-Components/Motherboards/All-series/PRIME-X570-PRO/HelpDesk_Download/")
     end
+end
+
+def checkForUpdates(toCheck)
+    return unless isCheckable(toCheck)
+
+    puts "\t- #{toCheck.upcase} -"
+
+    current = getCurrentVersion(toCheck)
+    newest = getNewestVersion(toCheck)
+    return if current == -1 or newest == -1
+    
+    if current.tr('.', '') < newest.tr('.', '') 
+        puts "There is a newer #{toCheck} available!"
+        puts "Current #{toCheck} version: #{current}."
+        puts "Newest #{toCheck} version: #{newest}."
+        puts "\n"
+        unless (isRelease)
+            puts "Warning! This is a beta version."
+        end
+		puts "\n"
+        return true
+    else
+        puts "You have the latest #{toCheck}.\n\n"
+    end
+end
+
+bios = checkForUpdates('bios')
+chipset = checkForUpdates('chipset')
+audiodriver = checkForUpdates('audiodriver')
+
+if bios or chipset or audiodriver
+    openBrowser
 end
